3484c81c7f9721c1df81a80b58941aa4f30095b4
Lokesh R <avulpakir19312015@gmail.com>
2026-09-24 23:44:34 +0000

    Add files via upload

--- ONE_UI_BLOATWARE_REMOVER.sh
+++ ONE_UI_BLOATWARE_REMOVER.sh
@@ -1,9 +1,103 @@
-#!/system/bin/sh
+#!/bin/sh
+
 
 echo "Starting test..."
 
 
 for package in \
+
+com.android.bookmarkprovider \
+com.android.hotwordenrollment.okgoogle \
+com.google.android.apps.googleassistant \
+com.google.android.apps.magazines \
+com.google.android.inputmethod.latin \
+com.google.android.inputmethod.pinyin \
+com.google.android.inputmethod.japanese \
+com.google.android.inputmethod.korean \
+com.google.android.apps.docs \
+com.google.android.apps.photos \
+com.google.android.apps.tachyon \
+com.google.android.apps.translate \
+com.google.android.apps.youtube \
+com.google.android.apps.books \
+com.google.android.feedback \
+com.google.android.apps.maps \
+com.google.android.projection.gearhead \
+com.google.android.apps.messaging \
+com.google.android.apps.walletnfcrel \
+com.google.android.apps.wallet \
+com.google.android.apps.youtube.music \
+com.google.android.apps.nbu.paisa.user \
+com.google.android.googlequicksearchbox \
+com.google.android.printservice.recommendation \
+com.google.android.apps.subscriptions.red \
+com.google.android.apps.podcasts \
+com.google.android.music \
+com.google.ar.lens \
+com.google.android.apps.wellbeing \
+com.android.printspooler \
+com.android.filemanager \
+com.android.notes \
+com.android.providers.partnerbookmarks \
+com.android.stk \
+com.android.stk2 \
+com.android.VideoPlayer \
+com.amazon.mShop.android.shopping \
+com.netflix.partner.activation \
+ru.yandex.searchplugin \
+com.ebay.mobile \
+com.ebay.carrier \
+com.alibaba.aliexpresshd \
+sg.bigo.live \
+com.samsung.android.bixby.wakeup \
+com.samsung.android.app.spage \
+com.samsung.android.app.routines \
+com.samsung.android.bixby.service \
+com.samsung.android.visionintelligence \
+com.samsung.android.bixby.agent \
+com.samsung.android.bixby.agent.dummy \
+com.samsung.android.bixbyvision.framework \
+com.samsung.android.messaging \
+com.sec.android.app.sbrowser \
+com.samsung.android.drivelink.stub \
+com.samsung.android.mateagent \
+com.sec.android.easyMover.Agent \
+com.samsung.android.app.watchmanagerstub \
+com.sec.android.daemonapp \
+com.samsung.android.app.social \
+com.samsung.ecomm.global \
+com.sec.android.app.voicenote \
+com.samsung.android.voc \
+com.sec.android.app.popupcalculator \
+com.samsung.android.oneconnect \
+com.samsung.android.widgetapp.yahooedge.finance \
+com.samsung.android.widgetapp.yahooedge.sport \
+com.samsung.app.highlightplayer \
+com.samsung.android.svoiceime \
+com.samsung.android.email.provider \
+com.samsung.android.wellbeing \
+com.samsung.android.service.livedrawing \
+com.sec.android.mimage.avatarstickers \
+com.samsung.android.game.gamehome \
+com.samsung.android.game.gametools \
+com.samsung.android.gametuner.thin \
+com.samsung.android.game.gos \
+com.samsung.android.hmt.vrsvc \
+com.sec.android.easyonehand \
+com.samsung.android.app.watchmanagerstub \
+com.samsung.android.service.aircommand \
+com.wsomacp \
+flipboard.boxer.app \
+com.samsung.android.aremoji \
+com.samsung.android.emojiupdater \
+com.samsung.android.app.camera.sticker.stamp.preload \
+com.samsung.android.stickercenter \
+com.samsung.android.app.vrsetupwizardstub \
+com.google.vr.vrcore \
+jp.gocro.smartnews.android \
+com.linkedin.android \
+com.samsung.android.app.contacts \
+com.cnn.mobile.android.phone \
 com.android.bookmarkprovider \
 com.android.egg \
 com.android.dreams.phototable \
@@ -97,6 +191,7 @@ com.samsung.android.game.gamehome \
 com.sec.android.gallery3d \
 com.sec.android.app.launcher \
 com.sec.android.easyMover 
+
 do
     echo "Uninstalling: $package"
     adb shell pm uninstall -k --user 0 "$package"
